113030e5fa756a380acc4a1c11569cc5b046d97d
Code_Hotaru <115505668+Renegade-3863@users.noreply.github.com>
2025-08-14 09:33:31 +0800

    清理了旧的不需要的杂项文件

--- src/storage/MySQLClient.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <drogon/orm/DbClient.h>
-
-// 使用 drogon 框架提供的 DbClient 类型实现与 MySQL 数据库的连接
-using MySQLClientPtr = std::shared_ptr<drogon::orm::DbClient>;
--- src/storage/RedisClient.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <drogon/nosql/RedisClient.h> 
-
-// 定义 RedisClient 类型
-using RedisClientPtr = std::shared_ptr<drogon::nosql::RedisClient>;
